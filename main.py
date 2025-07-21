3e9a7e839b91c777bd2a93899034f9f34c09a52f
Georg Gntuni <g.gntuni@gmail.com>
2025-07-21 15:44:39 +0000

    Changed structure in main and small details

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ import logging
 logger.setLevel(logging.DEBUG)
 
 # Material properties. Example inital values
-def _init(E=10e3, nu=0.3):
+def _init(E=50e3, nu=0.1):
     return E, nu
 
-def _init_problem(E=10e3, nu=0.3):
+def _init_problem(E=80e3, nu=0.45):
     return E, nu
 
 if __name__ == "__main__":
@@ -39,46 +39,42 @@ if __name__ == "__main__":
     mu = E / (2. * (1. + nu))
     lmbda = E * nu / ((1 + nu) * (1 - 2 * nu))  
     x0 = np.array([E, nu])
-    P0 = np.array([[1e8, 0], [0, 0.01]])
-    Q = np.array([[1e2, 0], [0, 1e-4]])
+    P0 = np.array([[1e8, 0], [0, 1e8]])
+    Q = np.array([[1e2, 0], [0, 1e2]])
     
     disp_var = 10
     stress_var = 1e3
-#    R = np.diag(np.concatenate([np.asarray([disp_var] * 186), np.asarray([stress_var] * 38)]))
-    R = np.diag(np.asarray([disp_var] * 1852))
+    R = np.diag(np.asarray([disp_var] * 3703))
     ekf = LinearElasticityEKF(Q=Q, R=R, x0=x0, P0=P0)
 
-    estimated_states = []
+    estimated_states = [_init()]
     measured_states = []
+    
     ### Solve problem and improve model
     i = 0
-    for _ in range(10):
-        x, _ = ekf.get_state()
-        E, nu = x
-        problem.set_material_parameters(E, nu)
-        #u, vm_stress = run_and_solve(problem=problem)
+    for _ in range(30):
+        # Init and run problem
+        problem_E, problem_nu = _init_problem()
+        problem.set_material_parameters(problem_E, problem_nu)
         u, _ = run_and_solve(problem=problem)
-        #z = np.concatenate([u, vm_stress])
-        z = u
-        # Varianz als fest annehmen
-        z = z + onp.random.normal(0, 1, size=z.shape)
+
+        # Run EKF
         ekf.predict()
-        ekf.update(z)
+        ekf.update(u)
         x1, _ = ekf.get_state()
-        w
         estimated_states.append(x1)
-        measured_states.append(x1_measured)
         i += 1
+        print(f"ITER {i}")
 
     estimated_E = [a[0] for a in estimated_states]
     estimated_nu = [a[1] for a in estimated_states]
-    measured_E = [a[0] for a in measured_states]
-    measured_nu = [a[1] for a in measured_states]
+    measured_E = [_init_problem()[0] for _ in estimated_E]
+    measured_nu = [_init_problem()[1] for _ in estimated_nu]
 
-    print(estimated_E)
-    print(measured_E)
+    print(estimated_nu)
+    print(measured_nu)
 
     plt.plot(estimated_E, color="red", label="EKF estimate")
     plt.plot(measured_E, color="blue", label="Simulation Measurement")
     plt.legend()
-    plt.savefig("plots/E6.pdf")
+    plt.savefig("plots/nu2.pdf")
