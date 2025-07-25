67385aacbf7ee78532fdc7c5c6ab311d185a2782
Sadegh Khalili Tehrani <s.khalili.t@gmail.com>
2025-07-25 17:29:24 +0200

    added penalty to nls residuals

--- nls/nls_optx.py
+++ nls/nls_optx.py
@@ -1,7 +1,33 @@
 import jax.numpy as jnp
 import optimistix as optx
-import tests_optx
 
+def lm_solver(init_params, epsilon, sigma_mes):
+    """
+    This function sets up and runs the Levenberg-Marquardt optimization solver using Optimistix.
+    It optimizes the parameters of a linear elasticity problem based on the residuals between
+    predicted and measured stress tensors.
+    Returns:
+        pred_params: The optimized parameters [E, nu] where E is Young's modulus and nu is Poisson's ratio.
+    """
+    
+    # Define the solver using Optimistix's Levenberg-Marquardt
+    solver = optx.LevenbergMarquardt(
+        rtol = 1e-8,
+        atol = 1e-8,
+        norm = optx.rms_norm
+    )
+
+    # Perform optimization
+    sol = optx.least_squares(residuals, solver, init_params, args=(epsilon, sigma_mes))
+
+    pred_params = sol.value
+
+    return pred_params
+
+# 1. Calculate stress from strain and estimated parameters
+# 2. Generate ground truth from Jax-FEM
+# 3. Calculate residuals
+# 4. Use Levenberg-Marquardt to optimize parameters
 
 def calculate_stress(epsilon, params):
     """ Calculate stress from strain and parameters.
@@ -32,34 +58,23 @@ def residuals(params, epsilon__sigma_mes):
     """
     # Unpack strain and measured stress
     epsilon, sigma_mes = epsilon__sigma_mes
+    # 
+    E = params[0]
+    nu = params[1]
     # Calculate predicted stress
     sigma_pred = calculate_stress(epsilon, params)
-    return sigma_pred - sigma_mes
 
+    res_sigma = sigma_pred - sigma_mes
 
-# Define the solver using Optimistix's Levenberg-Marquardt
-solver = optx.LevenbergMarquardt(
-    rtol = 1e-8,
-    atol = 1e-8,
-    norm = optx.rms_norm
-)
+    # Penalty for materialparmeters
+    if E < 0:
+        res_E = 1e3 * E**2
+    else:
+        res_E = 0
 
-# Initial guess for parameters [E, nu]
-initial_params = jnp.array([20000, 0.1])
+    if nu < 0 or nu > 0.5:
+        res_nu = 1e3 * nu**2
+    else:
+        res_nu = 0
 
-# Generate synthetic data
-epsilon, sigma_mes = tests_optx.generate_real_data()
-
-# Perform optimization
-sol = optx.least_squares(residuals, solver, initial_params, args=(epsilon, sigma_mes))
-
-pred_params = sol.value
-
-print("Estimated parameters:", pred_params)
-
-
-
-# 1. Calculate stress from strain and estimated parameters
-# 2. Generate ground truth from Jax-FEM
-# 3. Calculate residuals
-# 4. Use Levenberg-Marquardt to optimize parameters
+    return res_sigma + res_E + res_nu
